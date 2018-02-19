09a4e227ef59127767af807d0052f7f99c93bd15
John Griffin <griffinish@gmail.com>
2018-02-18 20:07:18 -0500

    added window-series.rkt

---
+++ window-series.rkt
@@ -0,0 +1,65 @@
+#lang racket/base
+
+(require "private/common-requirements.rkt")
+
+(provide
+ (contract-out
+  [ window-series (->* (S integer? (-> vector? real?))
+                       (#:dates DS
+                        #:missing-data-permitted boolean?)
+                       S)]))
+
+(define (window-series s N proc
+                       #:dates (dates (current-dates))
+                       #:missing-data-permitted (missing-data-permitted #f))
+  
+  (define window (make-vector N))
+  (define-values (dv vv fd out-v) (common-setup s dates))
+  (define first-slot (sub1 N))
+  (for ((dt (in-vector dv (sub1 N)))
+        (start-slot (in-naturals))
+        (stop-slot (in-naturals N)))
+    
+    (define all
+      (for/fold ((good #t))
+                ((val (in-vector vv start-slot stop-slot))
+                 (ndx (in-naturals)))
+        (vector-set! window ndx val)
+        (and good val)))
+    
+    (when (or all missing-data-permitted)
+      (vector-set! out-v (- dt fd) (proc window))))
+  
+  (make-vector-series
+   #:first-date fd
+   #:name (format "(window-series ~a ~a)" (abbreviate s) N)
+   #:vector out-v))
+
+
+;==================================================
+
+(module+ main 
+  (require "private/test-support.rkt"
+           "momentum.rkt")
+  (define MO1 (mo TEST-SERIES 1 #:dates (dates TEST-SERIES)))
+  (define (vector-sum v) (for/sum ((n (in-vector v))) n))
+  (typical-run (λ () (window-series MO1 200 vector-sum))))
+
+
+;==================================================
+
+(module+ test
+  (require rackunit
+           "sma.rkt"
+           "private/test-support.rkt")
+
+  
+  (check-not-exn
+   (λ ()
+     (with-dates TEST-SERIES
+       (verify-equivalency
+        (sma TEST-SERIES 200)
+        (window-series TEST-SERIES 200 (λ (v)
+                                        (/ (for/sum ((n (in-vector v))) n)
+                                           (vector-length v)))))))))
+
