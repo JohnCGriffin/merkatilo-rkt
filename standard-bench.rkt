3945c770cd8f3625ed7ebe088cc3c6e3df4f5d2f
John Griffin <griffinish@gmail.com>
2018-07-19 12:23:08 -0400

    fixed standard-bench

--- standard-bench.rkt
+++ standard-bench.rkt
@@ -8,7 +8,7 @@
 
 (define (standard-bench)
   
-  (define (nanos thunk)
+  (define (ops/second thunk)
     (define start (current-inexact-milliseconds))
     (define end
       (begin
@@ -19,11 +19,16 @@
               (for ((dt (in-vector (dateset-vector (current-dates)))))
                 (sf dt)))))
         (current-inexact-milliseconds)))
-    (define ms/op (/ (- end start) 100))
-    (inexact->exact (round (* ms/op 1000))))
+    (define seconds (/ (- end start) 1000))
+    (inexact->exact (round (/ 100 seconds))))
 
+  (define reusable-ema
+    (with-dates BENCHMARK-SERIES
+      (ema BENCHMARK-SERIES 10)))
   (define reusable-signal
-    (with-dates BENCHMARK-SERIES (ema BENCHMARK-SERIES 10)))
+    (with-dates BENCHMARK-SERIES
+      (to-signals (cross #:slower reusable-ema
+                         #:faster BENCHMARK-SERIES))))
   (define twos
     (constant 2.0))
 
@@ -31,25 +36,29 @@
     (sort
      (list
       (bench "prepend"
-             (λ () (prepend TEST-SERIES #:with-surrogate BENCHMARK-SERIES)))
-      (bench "fudge"
-             (let ((dts (dates (date-range '1980-1-1 '2020-1-1))))
-               (λ () (with-dates dts (fudge BENCHMARK-SERIES)))))
+             (λ () (prepend TEST-SERIES
+                            #:with-surrogate BENCHMARK-SERIES)))
+      (bench "fudge" (λ () (fudge BENCHMARK-SERIES)))
       (bench "min-max" (λ () (min-ob BENCHMARK-SERIES)))
       (bench "math-div" (λ () (div BENCHMARK-SERIES twos)))
       (bench "math-mul" (λ () (mul BENCHMARK-SERIES twos)))
+      (bench "math-sub" (λ () (sub BENCHMARK-SERIES twos)))
       (bench "math-add" (λ () (add BENCHMARK-SERIES twos)))
       (bench "signals" (λ () (to-signals reusable-signal)))
       (bench "volatility" (λ () (volatility BENCHMARK-SERIES)))
-      (bench "reversal" (λ () (reversals BENCHMARK-SERIES #:up-factor 1.11 #:down-factor 0.9)))
-      (bench "equity" (λ () (equity-line BENCHMARK-SERIES reusable-signal)))
+      (bench "reversal"
+             (λ () (reversals BENCHMARK-SERIES
+                              #:up-factor 1.11
+                              #:down-factor 0.9)))
+      (bench "equity"
+             (λ () (equity-line BENCHMARK-SERIES reusable-signal)))
       (bench "repeated" (λ () (repeated reusable-signal)))
       (bench "unrepeated" (λ () (unrepeated BENCHMARK-SERIES)))
       (bench "conviction" (λ () (conviction reusable-signal 2)))
       (bench "dd" (λ () (series-drawdown BENCHMARK-SERIES)))
       (bench "ema" (λ () (ema BENCHMARK-SERIES 10)))
       (bench "cross"
-             (λ () (cross #:slower reusable-signal
+             (λ () (cross #:slower reusable-ema
                           #:faster BENCHMARK-SERIES)))
       (bench "warp" (λ () (warp BENCHMARK-SERIES 10)))
       (bench "ma" (λ () (ma BENCHMARK-SERIES 10)))
@@ -60,9 +69,10 @@
 
   (with-dates BENCHMARK-SERIES
     (for ((b (in-list benchmarks)))
-      (let ((name (substring (format "~a              " (bench-name b)) 0 12))
-            (duration (nanos (bench-thunk b))))
-        (printf "~a\t~a\n" name duration)))))
+      (let ((name (substring (format "~a              "
+                                     (bench-name b)) 0 12))
+            (ops (ops/second (bench-thunk b))))
+        (printf "~a\t~a\n" name ops)))))
 
 (module+ main
   (require "private/test-support.rkt")
